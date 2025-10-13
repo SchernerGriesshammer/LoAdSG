1f13aba4f20583b22802b8da0fdb39f12a1c8196
SchernerGriesshammer <SchernerGriesshammer@users.noreply.github.com>
2025-10-13 08:45:38 +0000

    Update Doxygen HTML docs from main 79f5281322a68a3062032d1d97df0e155629b8b9

---
+++ class_adaptive_sparse_grid___base.js
@@ -0,0 +1,16 @@
+var class_adaptive_sparse_grid___base =
+[
+    [ "AdaptiveSparseGrid_Base", "class_adaptive_sparse_grid___base.html#a0333b0096daff8370254713989715d51", null ],
+    [ "getFreeSpaceNumberInSecondTable", "class_adaptive_sparse_grid___base.html#a29a6373d2c83b096a68d8b682cddba29", null ],
+    [ "getIndexOfTable", "class_adaptive_sparse_grid___base.html#a509b141399027ea802d3e86c3167d4ea", null ],
+    [ "hash", "class_adaptive_sparse_grid___base.html#a7452b162033326dad5e8f13f414d41d2", null ],
+    [ "occupied", "class_adaptive_sparse_grid___base.html#af33a2f3dd5047fc4731fa952308ce258", null ],
+    [ "Print_gnu", "class_adaptive_sparse_grid___base.html#a5e69df610de955884ad1cd0f394afcfa", null ],
+    [ "PrintActiveHanging", "class_adaptive_sparse_grid___base.html#afba92ad8bd28d0571f67e7d17b05e86c", null ],
+    [ "printCoordinates", "class_adaptive_sparse_grid___base.html#a97e0ca6bbcb3c87dad18d7442620e5da", null ],
+    [ "PrintGridIndices", "class_adaptive_sparse_grid___base.html#a9bead8f303d386701aeb525e25598e92", null ],
+    [ "indicesSecondTable", "class_adaptive_sparse_grid___base.html#ac13cac135ced9152aa44611950d813cc", null ],
+    [ "indicesSupportMin", "class_adaptive_sparse_grid___base.html#a7129b2c8f66f9a7498c5fb60c97aa703", null ],
+    [ "isActiveNodeTable", "class_adaptive_sparse_grid___base.html#a0bc1800357d1eb59606df9909f572845", null ],
+    [ "secondTable", "class_adaptive_sparse_grid___base.html#a8799522ad8b69f54a02f54ada24499bc", null ]
+];
---
+++ class_integrator_helm_constant.js
@@ -0,0 +1,4 @@
+var class_integrator_helm_constant =
+[
+    [ "stencil_integration", "class_integrator_helm_constant.html#a85e345cf338e6d29060b83d8ad4a09e0", null ]
+];
